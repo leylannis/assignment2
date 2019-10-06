237aac6d64fb852f218c70d53b5d793c873413f0
Leylanni Quijano-Shafer <loliface99@gmail.com>
2019-10-05 22:36:13 -0400

    Added test cases for new classes
    Each of the three new classes have two elements in the
    array in the driver program. Behavior will be printed.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "Car.h"
 #include "Bicycle.h"
+#include "jet.h"
+#include "skateboard.h"
+#include "train.h"
 
 void printVehiclesRoster(Vehicle **vehicles, int size);
 
@@ -16,6 +19,12 @@ int main() {
     vehiclesArray[3] = new Car("Tesla", "T2", "electricity", "large");
     vehiclesArray[4] = new Bicycle("Mizuno", "Wave", 10);
     vehiclesArray[5] = new Car("BMW", "X5", "diesel", "grande");
+    vehiclesArray[6] = new Jet();
+    vehiclesArray[7] = new Jet("Airbus", "A330", "unknown", 3);
+    vehiclesArray[8] = new Skateboard();
+    vehiclesArray[9] = new Skateboard("Flip", "HKD");
+    vehiclesArray[10] = new Train();
+    vehiclesArray[11] = new Train("CIWL", "Golden Arrow", "unknown", "passenger");
 
     printVehiclesRoster(vehiclesArray, size);
 
